d53a7931060f09f8602bb0da39660c16da62fb9b
n.semenov <apexkennys@gmail.com>
2023-12-05 13:45:51 +0300

    fixed: csv name

--- research/num2word/parsers/data_converter.py
+++ research/num2word/parsers/data_converter.py
@@ -7,7 +7,7 @@ from sentence_splitter.splitter import SyntaxAnalyzer
 sa = SyntaxAnalyzer()
 ne = NumberExtractor()
 
-part_1 = pd.read_csv('data/lenta-ru-base.csv')
+part_1 = pd.read_csv('data/lenta-ru-news.csv')
 part_2 = pd.read_csv('data/from_15-12-2019.csv')
 
 df = pd.concat(objs=[part_1, part_2])
